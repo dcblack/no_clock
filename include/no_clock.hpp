43fb042f705ad9a8ea8fc954e17af9921e8eebbc
David C Black <david.black@doulos.com>
2020-09-15 05:33:19 -0500

    Modern C++ changes. Also move from .h to .hpp to indicate exclusiveness of headers for C++

--- include/no_clock.hpp
+++ include/no_clock.hpp
@@ -1,5 +1,5 @@
-#ifndef NONCLOCK_H
-#define NONCLOCK_H
+#ifndef NONCLOCK_HPP
+#define NONCLOCK_HPP
 
 ///////////////////////////////////////////////////////////////////////////////
 // $License: Apache 2.0 $
@@ -18,60 +18,12 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-// DESCRIPTION
-//   The no_clock object simplifies high-level modeling of clocks without
-//   incurring the context switching overhead of a real clock.  It does so by
-//   providing a number of methods that calculate timing offsets and reducing
-//   multiple clock delays to a single wait.  This contrasts with traditional
-//   clocks that issues millions of unused cycles that slow down simulation due
-//   to the overhead of context switching.
-//
-//   To simplify the usage model, no_clock is designed to have a similar syntax
-//   to a real sc_clock. This should make it easier for designers to adopt. In
-//   fact, you should be able to replace no_clock with sc_clock at any time as
-//   the design is refined without much change to the underlying code.
-//
-//   New concepts include sampling and setting time offsets. This simplifies
-//   modeling setup & hold rules and aids verification.
-//
-//   Below are declarations and corresponding timing diagrams that may be useful
-//   to understanding the design.  We also provide a number of utility methods
-//   in the no_clock class as an aid to modeling.
-//
-//   [Ed: The #ifdef allows syntax coloring/checking (in vim) only. Never define NEVER.]
-#ifdef NEVER /* DOCUMENTATION */
-#error This code should NEVER be enabled
-     const sc_core::sc_time(1.0,SC_NS) ns;
-     no_clock CLK1("CLK1",/*period*/10*ns,/*duty*/0.5,/*offset*/0*ns,/*1stpos*/true ,/*smpl*/1*ns,/*chg*/5*ns);
-     no_clock CLK2("CLK2",/*period*/12*ns,/*duty*/0.3,/*offset*/1*ns,/*1stpos*/false,/*smpl*/7*ns,/*chg*/9*ns);
-#endif
-//   Examples of above definitions impact to the virtual clocks they define.
-//   |                                |                                    |
-//   |       _0123456789_123456789_1  |       _123456789_123456789_123456  |
-//   |        :____     :____     :_  |       _:        ___:        ___:   |
-//   |  CLK1 _|    |____|    |____|   |  CLK2  |_______|:  |_______|:  |_  |
-//   |        :    :    :    :    :   |        :  :  :     :  :  :     :   |
-//   |  DATA  :s   c    :s   c    :   |        :  s  c     :  s  c     :   |
-//   |        ::   :    ::   :    :   |        :  :  :     :  :  :     :   |
-//   |  Time  0:   :   10:   :   20   |  Time  1      8 10 13    20 22 25  |
-//   |         1   5    11  15        |        :<--7->: :                  |
-//   |                                |        :<---9-->:                  |
-//
-//   In diagram, (s)ample and (c)hange are simply abbreviations.
-//
-//   Finally, we implement the concept of global clocks as a convenience. This
-//   allows modeling without the burden of connecting clocks, which may be
-//   deferred to implementation. Each instantiation should maintain local
-//   pointers to the global clocks it uses.
-//
-//   Note that no_clock does not inherit from sc_object, and may therefor be
-//   instantiated at anytime. This is unlike sc_core::sc_clock, which is a
-//   proper piece of SystemC hardware.
+// Description: See ABOUT_TIMER.md
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "no_clock_if.h"
-#include <systemc>
+#define MSGID "/Doulos/no_clock"
+#include "no_clock_if.hpp"
 #include <map>
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -117,17 +69,18 @@ sc_core::sc_time sc_core_time_diff
 )
 {
   if (lhs >= rhs) return (lhs - rhs);
+  static char const * const msgid { "/Doulos/Example/time_diff" };
 #ifdef INVERT_NEGATIVE
   sc_core::sc_report_handler::report
     ( sc_core::SC_WARNING
-    , "time_diff"
+    , msgid
     , "Negative time calculation returning the negative difference"
     , file, lnum);
   return (rhs - lhs);
 #else
   sc_core::sc_report_handler::report
     ( sc_core::SC_WARNING
-    , "time_diff"
+    , msgid
     , "Negative time calculation returning SC_ZERO_TIME"
     , file, lnum);
   return sc_core::SC_ZERO_TIME;
@@ -218,32 +171,32 @@ public:
   void set_setedge_time       ( sc_core::sc_time tSETEDGE   );
   void set_time_shift         ( sc_core::sc_time tSHIFT    );
   const char*      name       ( void ) const;
-  sc_core::sc_time period     ( unsigned int cycles = 1 ) const;
+  sc_core::sc_time period     ( Clock_count_t cycles = 1 ) const;
   double           duty       ( void ) const;
   double           frequency  ( void ) const;
   sc_core::sc_time time_shift ( void ) const { return m_tSHIFT; }
   // Special conveniences
-  unsigned int     cycles ( void ) const; // Number of clock cycles since start
+  Clock_count_t     cycles ( void ) const; // Number of clock cycles since start
   void             reset  ( void ); // Clears count & frequency change base (not yet implemented -- specification issues remain)
-  unsigned int     frequency_changes ( void ) const { return m_freq_count; } // Number of times frequency was changed
+  Clock_count_t     frequency_changes ( void ) const { return m_freq_count; } // Number of times frequency was changed
   // Calculate the delay till... (use for temporal offset)...may return SC_ZERO_TIME if already on the edge
-  sc_core::sc_time  until_posedge ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  until_negedge ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  until_anyedge ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  until_sample  ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  until_setedge ( unsigned int cycles = 0U ) const;
+  sc_core::sc_time  until_posedge ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  until_negedge ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  until_anyedge ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  until_sample  ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  until_setedge ( Clock_count_t cycles = 0U ) const;
   // Calculate the delay till... (use for temporal offset)...never returns SC_ZERO_TIME
-  sc_core::sc_time  next_posedge ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  next_negedge ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  next_anyedge ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  next_sample  ( unsigned int cycles = 0U ) const;
-  sc_core::sc_time  next_setedge ( unsigned int cycles = 0U ) const;
+  sc_core::sc_time  next_posedge ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  next_negedge ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  next_anyedge ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  next_sample  ( Clock_count_t cycles = 0U ) const;
+  sc_core::sc_time  next_setedge ( Clock_count_t cycles = 0U ) const;
   // Wait only if really necessary (for use in SC_THREAD)
-  void wait_posedge ( unsigned int cycles = 0U );
-  void wait_negedge ( unsigned int cycles = 0U );
-  void wait_anyedge ( unsigned int cycles = 0U );
-  void wait_sample  ( unsigned int cycles = 0U );
-  void wait_setedge ( unsigned int cycles = 0U );
+  void wait_posedge ( Clock_count_t cycles = 0U );
+  void wait_negedge ( Clock_count_t cycles = 0U );
+  void wait_anyedge ( Clock_count_t cycles = 0U );
+  void wait_sample  ( Clock_count_t cycles = 0U );
+  void wait_setedge ( Clock_count_t cycles = 0U );
   // Are we there? (use in SC_METHOD)
   bool at_posedge_time ( void ) const;
   bool posedge         ( void ) const { return at_posedge_time(); }
@@ -255,15 +208,17 @@ public:
   bool at_setedge_time ( void ) const;
   // For compatibility if you really have/want to (with an extra feature)
   // - specify N > 0 to delay further out
-  sc_core::sc_event& default_event       ( size_t events = 0 );
-  sc_core::sc_event& posedge_event       ( size_t events = 0 );
-  sc_core::sc_event& negedge_event       ( size_t events = 0 );
-  sc_core::sc_event& sample_event        ( size_t events = 0 );
-  sc_core::sc_event& setedge_event       ( size_t events = 0 );
-  sc_core::sc_event& value_changed_event ( size_t events = 0 );
-  bool      read                ( void ) const;
+  sc_core::sc_event& default_event       ( size_t events = 0 ) override;
+  sc_core::sc_event& posedge_event       ( size_t events = 0 ) override;
+  sc_core::sc_event& negedge_event       ( size_t events = 0 ) override;
+  sc_core::sc_event& sample_event        ( size_t events = 0 ) override;
+  sc_core::sc_event& setedge_event       ( size_t events = 0 ) override;
+  sc_core::sc_event& value_changed_event ( size_t events = 0 ) override;
+  bool      read                ( void ) const override;
 
-  virtual void write( const bool& ) { SC_REPORT_ERROR("/no_clock","write() not allowed on clock"); }
+  virtual void write( const bool& ) { SC_REPORT_ERROR(MSGID,"write() not allowed on clock"); }
+  virtual sc_core::sc_time delay( sc_core::sc_time tPERIOD, sc_core::sc_time tOFFSET, sc_core::sc_time tSHIFT) const override;
+  virtual Clock_count_t clocks( sc_core::sc_time tPERIOD, sc_core::sc_time tZERO, sc_core::sc_time tSHIFT) const override;
 
 private:
   // Don't allow copying
@@ -338,100 +293,100 @@ inline unsigned long int clocks
 // Accessors
 //------------------------------------------------------------------------------
 inline const char*      no_clock::name      ( void ) const { return m_clock_name; }
-inline sc_core::sc_time no_clock::period    ( unsigned int cycles ) const { return cycles*m_tPERIOD; }
+inline sc_core::sc_time no_clock::period    ( Clock_count_t cycles ) const { return cycles*m_tPERIOD; }
 inline double           no_clock::duty      ( void ) const { return m_duty; }
 inline double           no_clock::frequency ( void ) const { return sc_core::sc_time(1,sc_core::SC_SEC)/m_tPERIOD; }
 
 //------------------------------------------------------------------------------
 // Special conveniences
 //------------------------------------------------------------------------------
-inline unsigned int     no_clock::cycles ( void ) const // Number of clock cycles since start
+inline Clock_count_t     no_clock::cycles ( void ) const // Number of clock cycles since start
 {
   return m_base_count + clocks(m_tPERIOD,m_frequency_set,m_tSHIFT);
 }
 
 // Calculate the delay till... (use for temporal offset)
-inline sc_core::sc_time  no_clock::until_posedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::until_posedge ( Clock_count_t cycles ) const
 {
   return cycles*m_tPERIOD+delay(m_tPERIOD,m_tPOSEDGE,m_tSHIFT);
 }
 
-inline sc_core::sc_time  no_clock::until_negedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::until_negedge ( Clock_count_t cycles ) const
 {
   return cycles*m_tPERIOD+delay(m_tPERIOD,m_tNEGEDGE,m_tSHIFT);
 }
 
-inline sc_core::sc_time  no_clock::until_anyedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::until_anyedge ( Clock_count_t cycles ) const
 {
   return cycles*m_tPERIOD+(true==read()?until_negedge():until_posedge());
 }
 
-inline sc_core::sc_time  no_clock::until_sample  ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::until_sample  ( Clock_count_t cycles ) const
 {
   return cycles*m_tPERIOD+delay(m_tPERIOD,m_tSAMPLE,m_tSHIFT);
 }
 
-inline sc_core::sc_time  no_clock::until_setedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::until_setedge ( Clock_count_t cycles ) const
 {
   return cycles*m_tPERIOD+delay(m_tPERIOD,m_tSETEDGE,m_tSHIFT);
 }
 
 // Calculate the delay till next... (use for temporal offset) - never returns 0
-inline sc_core::sc_time  no_clock::next_posedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::next_posedge ( Clock_count_t cycles ) const
 {
   sc_core::sc_time t(delay(m_tPERIOD,m_tPOSEDGE,m_tSHIFT));
   return (cycles + (sc_core::SC_ZERO_TIME == t?1:0)) * m_tPERIOD + t;
 }
 
-inline sc_core::sc_time  no_clock::next_negedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::next_negedge ( Clock_count_t cycles ) const
 {
   sc_core::sc_time t(delay(m_tPERIOD,m_tNEGEDGE,m_tSHIFT));
   return (cycles + (sc_core::SC_ZERO_TIME == t?1:0)) * m_tPERIOD + t;
 }
 
-inline sc_core::sc_time  no_clock::next_anyedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::next_anyedge ( Clock_count_t cycles ) const
 {
   return cycles*m_tPERIOD+(true==read()?next_negedge():next_posedge());
 }
 
-inline sc_core::sc_time  no_clock::next_sample  ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::next_sample  ( Clock_count_t cycles ) const
 {
   sc_core::sc_time t(delay(m_tPERIOD,m_tSAMPLE,m_tSHIFT));
   return (cycles + (sc_core::SC_ZERO_TIME == t?1:0)) * m_tPERIOD + t;
 }
 
-inline sc_core::sc_time  no_clock::next_setedge ( unsigned int cycles ) const
+inline sc_core::sc_time  no_clock::next_setedge ( Clock_count_t cycles ) const
 {
   sc_core::sc_time t(delay(m_tPERIOD,m_tSETEDGE,m_tSHIFT));
   return (cycles + (sc_core::SC_ZERO_TIME == t?1:0)) * m_tPERIOD + t;
 }
 
 // Wait only if really necessary (for use in SC_THREAD) -- may be a NOP if cycles == 0
-inline void no_clock::wait_posedge ( unsigned int cycles )
+inline void no_clock::wait_posedge ( Clock_count_t cycles )
 {
   sc_core::sc_time t(until_posedge(cycles));
   if (sc_core::SC_ZERO_TIME != t) wait(t);
 }
 
-inline void no_clock::wait_negedge ( unsigned int cycles )
+inline void no_clock::wait_negedge ( Clock_count_t cycles )
 {
   sc_core::sc_time t(until_negedge(cycles));
   if (sc_core::SC_ZERO_TIME != t) wait(t);
 }
 
-inline void no_clock::wait_anyedge ( unsigned int cycles )
+inline void no_clock::wait_anyedge ( Clock_count_t cycles )
 {
   sc_core::sc_time t(until_anyedge(cycles));
   if (sc_core::SC_ZERO_TIME != t) wait(t);
 }
 
-inline void no_clock::wait_sample  ( unsigned int cycles )
+inline void no_clock::wait_sample  ( Clock_count_t cycles )
 {
   sc_core::sc_time t(until_sample(cycles));
   if (sc_core::SC_ZERO_TIME != t) wait(t);
 }
 
-inline void no_clock::wait_setedge ( unsigned int cycles )
+inline void no_clock::wait_setedge ( Clock_count_t cycles )
 {
   sc_core::sc_time t(until_setedge(cycles));
   if (sc_core::SC_ZERO_TIME != t) wait(t);
--- include/no_clock_if.hpp
+++ include/no_clock_if.hpp
@@ -1,5 +1,5 @@
-#ifndef NONCLOCK_IF_H
-#define NONCLOCK_IF_H
+#ifndef NONCLOCK_IF_HPP
+#define NONCLOCK_IF_HPP
 
 ///////////////////////////////////////////////////////////////////////////////
 // $License: Apache 2.0 $
@@ -17,11 +17,14 @@
 // limitations under the License.
 
 #include <systemc>
+#include <cstdint>
 
 // This interface allows the use of no_clock with SystemC ports - see no_clock.h for more information
 
-class no_clock_if
-: public sc_core::sc_interface
+typedef uint64_t Clock_count_t;
+
+struct no_clock_if
+: virtual sc_core::sc_interface
 {
   virtual void               set_frequency       ( double           frequency ) = 0;
   virtual void               set_period_time     ( sc_core::sc_time period    ) = 0;
@@ -29,31 +32,33 @@ class no_clock_if
   virtual void               set_duty_cycle      ( double           duty      ) = 0;
   virtual void               set_sample_time     ( sc_core::sc_time sample    ) = 0;
   virtual void               set_setedge_time    ( sc_core::sc_time setedge   ) = 0;
-  virtual const char*        name                ( void ) const = 0;
-  virtual sc_core::sc_time   period              ( unsigned int cycles = 0U ) const = 0;
+  virtual const char*        clock_name          ( void ) const = 0;
+  virtual sc_core::sc_time   period              ( Clock_count_t cycles = 0U ) const = 0;
   virtual double             duty                ( void ) const = 0;
   virtual double             frequency           ( void ) const = 0;
   // Special conveniences
-  virtual unsigned int       cycles              ( void ) const = 0; // Number of clock cycles since start
-  virtual unsigned int       frequency_changes   ( void ) const = 0; // Number of times frequency was changed
+  virtual Clock_count_t      cycles              ( void ) const = 0; // Number of clock cycles since start
+  virtual Clock_count_t      cycles              ( sc_core::sc_time t ) const = 0;
+  virtual Clock_count_t      frequency_changes   ( void ) const = 0; // Number of times frequency was changed
   // Calculate the delay till... (use for temporal offset)...may return SC_ZERO_TIME if already on the edge
-  virtual sc_core::sc_time   until_posedge       ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   until_negedge       ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   until_anyedge       ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   until_sample        ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   until_setedge       ( unsigned int cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   until_posedge       ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   until_negedge       ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   until_anyedge       ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   until_sample        ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   until_setedge       ( Clock_count_t cycles = 0U ) const = 0;
   // Calculate the delay till... (use for temporal offset)...never returns SC_ZERO_TIME
-  virtual sc_core::sc_time   next_posedge       ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   next_negedge       ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   next_anyedge       ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   next_sample        ( unsigned int cycles = 0U ) const = 0;
-  virtual sc_core::sc_time   next_setedge       ( unsigned int cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   next_posedge       ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   next_negedge       ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   next_anyedge       ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   next_sample        ( Clock_count_t cycles = 0U ) const = 0;
+  virtual sc_core::sc_time   next_setedge       ( Clock_count_t cycles = 0U ) const = 0;
   // Wait only if really necessary (for use in SC_THREAD)
-  virtual void               wait_posedge        ( unsigned int cycles = 0U ) = 0;
-  virtual void               wait_negedge        ( unsigned int cycles = 0U ) = 0;
-  virtual void               wait_anyedge        ( unsigned int cycles = 0U ) = 0;
-  virtual void               wait_sample         ( unsigned int cycles = 0U ) = 0;
-  virtual void               wait_setedge        ( unsigned int cycles = 0U ) = 0;
+  virtual void               wait                ( Clock_count_t cycles = 0U ) = 0;
+  virtual void               wait_posedge        ( Clock_count_t cycles = 0U ) = 0;
+  virtual void               wait_negedge        ( Clock_count_t cycles = 0U ) = 0;
+  virtual void               wait_anyedge        ( Clock_count_t cycles = 0U ) = 0;
+  virtual void               wait_sample         ( Clock_count_t cycles = 0U ) = 0;
+  virtual void               wait_setedge        ( Clock_count_t cycles = 0U ) = 0;
   // Are we there? (use in SC_METHOD)
   virtual bool               at_posedge_time     ( void ) const = 0;
   virtual bool               posedge             ( void ) const = 0;
@@ -71,5 +76,8 @@ class no_clock_if
   virtual const sc_core::sc_event& setedge_event       ( size_t events = 0 ) = 0;
   virtual const sc_core::sc_event& value_changed_event ( size_t events = 0 ) = 0;
   virtual bool               read                ( void ) const = 0;
+  // Utility
+  virtual sc_core::sc_time delay( sc_core::sc_time tPERIOD, sc_core::sc_time tOFFSET=sc_core::SC_ZERO_TIME , sc_core::sc_time tSHIFT=sc_core::SC_ZERO_TIME) const = 0;
+  virtual Clock_count_t clocks( sc_core::sc_time tPERIOD, sc_core::sc_time tZERO=sc_core::SC_ZERO_TIME, sc_core::sc_time tSHIFT=sc_core::SC_ZERO_TIME) const = 0;
 };
 #endif
